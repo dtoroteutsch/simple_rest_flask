652a78ce42867ed7a7c3255aff65aafcf0ce9a7d
Diego <toro.teutsch.d@gmail.com>
2020-06-02 20:02:10 -0400

    Implementing error handling when the resource not exist

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, g, jsonify
+from flask import Flask, g, jsonify, abort
 
 from models import initialize
 from models import Course
@@ -20,12 +20,28 @@ def after_request(request):
     g.db.close()
     return request
 
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify(generate_response(404, error='Resource not found'))
+
 @app.route('/rest/api/v1.0/courses', methods=['GET'])
 def get_courses():
     #select * from courses;
     courses = Course.select()
     courses = [course.to_json() for course in courses]
     return jsonify(generate_response(data=courses))
+
+@app.route('/rest/api/v1.0/courses/<int:course_id>', methods=['GET'])
+def show_course(course_id):
+    course = get_course(course_id)
+    return jsonify(generate_response(data=course.to_json()))
+
+def get_course(course_id):
+    try:
+        #select * from courses where courses.id = course_id
+        return Course.get(Course.id == course_id)
+    except Course.DoesNotExist:
+        abort(404)
 
 def generate_response(status=200, data=None, error=None):
     return {
